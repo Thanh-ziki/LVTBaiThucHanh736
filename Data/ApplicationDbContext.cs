dabfcddcd2f226abf81cb8ba5fbecc96cbe151a3
Thanh-ziki <thanh.luongvan2k@gmail.com>
2023-07-24 16:51:59 +0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+namespace MvcMovie.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; } = default!;
+    }
+
+}
+    
